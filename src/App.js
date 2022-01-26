6b2a1e6f9232761b43fb8563d93f7d32d695cbfc
mig rod <35626373+guelmi92@users.noreply.github.com>
2022-01-26 15:18:27 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -122,7 +122,7 @@ function App() {
   const blockchain = useSelector((state) => state.blockchain);
   const data = useSelector((state) => state.data);
   const [claimingNft, setClaimingNft] = useState(false);
-  const [feedback, setFeedback] = useState(`Click buy to mint your NFT.`);
+  const [feedback, setFeedback] = useState(`Click Mint to grab your NFT.`);
   const [_mintAmount, setMintAmount] = useState(1);
   const [CONFIG, SET_CONFIG] = useState({
     CONTRACT_ADDRESS: "",
